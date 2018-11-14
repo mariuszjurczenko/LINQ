b184c9fc885c0e4dedea5c89fc09d15931c99e7a
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-11-14 09:53:17 +0100

    1_2 Wyswietlanie plików z wybranegokatalogu bez użycia LINQ

--- 1 Wprowadzenie/Program.cs
+++ 1 Wprowadzenie/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,30 @@ namespace _1_Wprowadzenie
     {
         static void Main(string[] args)
         {
+            var sciezka = @"c:\windows";
+            PokazDuzePlikiBezLinq(sciezka);
+        }
+
+        private static void PokazDuzePlikiBezLinq(string sciezka)
+        {
+            DirectoryInfo katalog = new DirectoryInfo(sciezka);
+            FileInfo[] pliki = katalog.GetFiles();
+
+            Array.Sort(pliki, new FileInfoComparer());
+
+            for (var i = 0; i < 5; i++)
+            {
+                FileInfo plik = pliki[i];
+                Console.WriteLine($"{plik.Name, -20} : {plik.Length, 20:N0}");
+            }
+        }
+    }
+
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
         }
     }
 }
